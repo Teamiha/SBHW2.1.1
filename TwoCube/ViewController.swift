21813060afc419bc9a417347082574536b7eb283
Teamiha <jegnum@gmail.com>
2022-01-28 23:07:50 +0300

    Initial Commit

---
+++ TwoCube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoCube
+//
+//  Created by Михаил Светлов on 28.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
